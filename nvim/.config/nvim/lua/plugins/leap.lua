046804756c9ffc3c0d83096f33041adb76571376
Daniel Cox <63171098+dan-kc@users.noreply.github.com>
2023-03-31 18:58:17 +0100

    Configured leap and removed flit

---
+++ nvim/.config/nvim/lua/plugins/leap.lua
@@ -0,0 +1,28 @@
+return {
+  "ggandor/leap.nvim",
+  keys = {
+    { "s", mode = { "n", "x", "o" }, desc = "Leap forward to" },
+    { "S", mode = { "n", "x", "o" }, desc = "Leap backward to" },
+    { "gs", mode = { "n", "x", "o" }, desc = "Leap from windows" },
+    {
+      "<leader><space>",
+      mode = { "n", "x", "o" },
+      function()
+        local focusable_windows_on_tabpage = vim.tbl_filter(function(win)
+          return vim.api.nvim_win_get_config(win).focusable
+        end, vim.api.nvim_tabpage_list_wins(0))
+        require("leap").leap({ target_windows = focusable_windows_on_tabpage })
+      end,
+      desc = "Leap anywhere",
+    },
+  },
+  config = function(_, opts)
+    local leap = require("leap")
+    for k, v in pairs(opts) do
+      leap.opts[k] = v
+    end
+    leap.add_default_mappings(true)
+    vim.keymap.del({ "x", "o" }, "x")
+    vim.keymap.del({ "x", "o" }, "X")
+  end,
+}
