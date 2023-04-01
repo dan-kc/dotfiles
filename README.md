ad0899cb9e0032d89ad6861b518b2551f37a1e7b
Daniel Cox <63171098+dan-kc@users.noreply.github.com>
2023-04-01 19:34:16 +0100

    Add delay to which key

--- README.md
+++ README.md
@@ -4,6 +4,5 @@
 
 - Add autoinstall script to homebrew
 - Configure lsp such that emmet is last suggested
-- Add delay to which-key
 - Change 'proj' function to cd into directory
 - Change theme why not
---
+++ nvim/.config/nvim/lua/plugins/which-key.lua
@@ -0,0 +1,37 @@
+local Util = require("lazyvim.util")
+return {
+  "folke/which-key.nvim",
+  event = "VeryLazy",
+  opts = {
+    plugins = { spelling = true },
+  },
+  config = function(_, opts)
+    local wk = require("which-key")
+    vim.o.timeout = true
+    vim.o.timeoutlen = 1600
+    wk.setup(opts)
+    local keymaps = {
+      mode = { "n", "v" },
+      ["g"] = { name = "+goto" },
+      ["gz"] = { name = "+surround" },
+      ["]"] = { name = "+next" },
+      ["["] = { name = "+prev" },
+      ["<leader><tab>"] = { name = "+tabs" },
+      ["<leader>b"] = { name = "+buffer" },
+      ["<leader>c"] = { name = "+code" },
+      ["<leader>f"] = { name = "+file/find" },
+      ["<leader>g"] = { name = "+git" },
+      ["<leader>gh"] = { name = "+hunks" },
+      ["<leader>h"] = { name = "+harpoon" },
+      ["<leader>q"] = { name = "+quit/session" },
+      ["<leader>s"] = { name = "+search" },
+      ["<leader>u"] = { name = "+ui" },
+      ["<leader>w"] = { name = "+windows" },
+      ["<leader>x"] = { name = "+diagnostics/quickfix" },
+    }
+    if Util.has("noice.nvim") then
+      keymaps["<leader>sn"] = { name = "+noice" }
+    end
+    wk.register(keymaps)
+  end,
+}
