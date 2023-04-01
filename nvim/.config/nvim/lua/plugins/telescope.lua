df4e8c465df490ae31ebaff47fb42b58c43e5e4d
Daniel Cox <63171098+dan-kc@users.noreply.github.com>
2023-04-01 19:35:36 +0100

    better telescope find files binding

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -1,7 +1,10 @@
+local Util = require("lazyvim.util")
 return {
   "nvim-telescope/telescope.nvim",
   keys = {
     -- disable the keymap to grep files
     { "<leader><space>", false },
+    { "<leader>fF", Util.telescope("files"), desc = "Find Files (root dir)" },
+    { "<leader>ff", Util.telescope("files", { cwd = false }), desc = "Find Files (cwd)" },
   },
 }
