3e1e7416f12290f342798311b57d577f4506b4bb
Daniel Cox <63171098+dan-kc@users.noreply.github.com>
2023-04-01 21:26:51 +0100

    Added flit back I miss it

--- nvim/.config/nvim/lua/plugins/disable.lua
+++ nvim/.config/nvim/lua/plugins/disable.lua
@@ -3,5 +3,4 @@ return {
   { "akinsho/bufferline.nvim", enabled = false },
   { "rcarriga/nvim-notify", enabled = false },
   { "folke/noice.nvim", enabled = false },
-  { "ggandor/flit.nvim", enabled = false },
 }
