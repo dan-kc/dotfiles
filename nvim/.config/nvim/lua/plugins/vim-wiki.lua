44eadf960dbc51178e5e6ec1c1d079680fb80e8a
Daniel Cox <63171098+dan-kc@users.noreply.github.com>
2023-04-27 21:07:23 +0100

    Added vim-wiki

---
+++ nvim/.config/nvim/lua/plugins/vim-wiki.lua
@@ -0,0 +1,16 @@
+return {
+  "vimwiki/vimwiki",
+  init = function()
+    vim.g.vimwiki_global_ext = 0
+    vim.g.vimwiki_list = {
+      {
+        path = "~/notes/wiki/",
+        syntax = "markdown",
+        ext = ".md",
+      },
+    }
+  end,
+  keys = {
+    { "<leader>n", "<cmd>VimwikiIndex<cr>", desc = "Notes index" },
+  },
+}
