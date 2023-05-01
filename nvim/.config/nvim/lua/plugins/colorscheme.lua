7690870fdda52e2b3b0348e1a5457e152b516d84
Daniel Cox <63171098+dan-kc@users.noreply.github.com>
2023-05-01 21:03:46 +0100

    changed theme to transparent
    change nvim theme

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -2,14 +2,16 @@ return {
   -- Install colorschemes
   { "ellisonleao/gruvbox.nvim" },
   { "catppuccin/nvim" },
+  { "Tsuzat/NeoSolarized.nvim" },
   { "shaunsingh/oxocarbon.nvim" },
   { "rose-pine/neovim" },
+  { "folke/tokyonight.nvim" },
 
   -- Set default colorscheme
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "rose-pine-moon",
+      colorscheme = "NeoSolarized",
     },
   },
 }
