19eaae00e04d8f814e278c22fd5328d6fa86f4ba
Daniel Cox <63171098+dan-kc@users.noreply.github.com>
2023-05-01 10:12:43 +0100

    added back yabai and skhd

--- nvim/.config/nvim/lua/config/autocmds.lua
+++ nvim/.config/nvim/lua/config/autocmds.lua
@@ -1,3 +1,14 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+--
+
+vim.api.nvim_create_autocmd("BufWritePost", {
+  pattern = { ".yabairc" },
+  command = "!brew services restart yabai",
+})
+
+vim.api.nvim_create_autocmd("BufWritePost", {
+  pattern = { ".skhdrc" },
+  command = "!brew services restart skhd",
+})
