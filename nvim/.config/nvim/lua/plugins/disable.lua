fec92cf893da402f6a4b811efa93a93dce0b3627
Daniel Cox <63171098+dan-kc@users.noreply.github.com>
2023-05-03 11:34:28 +0100

    replace spectre with bqf

--- nvim/.config/nvim/lua/plugins/disable.lua
+++ nvim/.config/nvim/lua/plugins/disable.lua
@@ -3,4 +3,5 @@ return {
   { "akinsho/bufferline.nvim", enabled = false },
   { "rcarriga/nvim-notify", enabled = false },
   { "folke/noice.nvim", enabled = false },
+  { "nvim-pack/nvim-spectre", enabled = false },
 }
