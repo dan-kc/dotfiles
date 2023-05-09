c42bf35b99616368b45a713fe72806703d604858
Daniel Cox <63171098+dan-kc@users.noreply.github.com>
2023-05-09 23:29:18 +0100

    listed bugs with colorschemes

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,11 +1,15 @@
 return {
   -- Install colorschemes
-  { "ellisonleao/gruvbox.nvim" },
-  { "catppuccin/nvim" },
+  { "ellisonleao/gruvbox.nvim" }, -- Bug: Git highlight opaque background
+  { "catppuccin/nvim" }, -- <3
   { "Tsuzat/NeoSolarized.nvim" },
-  { "shaunsingh/oxocarbon.nvim" },
-  { "rose-pine/neovim" },
-  { "folke/tokyonight.nvim" },
+  { "shaunsingh/oxocarbon.nvim" }, --Bug: Git highlight opaque background | Telescope looks kinda dead
+  { "rose-pine/neovim" }, --Bug: Git highlight opaque background
+  { "folke/tokyonight.nvim" }, -- Bug: doesn't wanna install??
+  { "JoosepAlviste/palenightfall.nvim" }, -- Telescope opaque background
+  { "rebelot/kanagawa.nvim" }, -- Bug: Telescope border chunky
+  { "Yagua/nebulous.nvim" }, -- Bug: Telescope highlight missing
+  { "marko-cerovac/material.nvim" }, -- Bug: Telescope highlight missing
 
   -- Set default colorscheme
   {
