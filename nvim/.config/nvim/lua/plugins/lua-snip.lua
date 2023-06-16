b3b43e31e58b8fe4773c58b0923e33d55d37152b
Daniel Cox <63171098+dan-kc@users.noreply.github.com>
2023-06-16 20:15:27 +0100

    lsp cleanup

--- nvim/.config/nvim/lua/plugins/lua-snip.lua
+++ nvim/.config/nvim/lua/plugins/lua-snip.lua
@@ -1,21 +1,9 @@
 -- Remove the Tab functionality from the default config
-
 return {
-  "L3MON4D3/LuaSnip",
-  build = (not jit.os:find("Windows"))
-      and "echo -e 'NOTE: jsregexp is optional, so not a big deal if it fails to build\n'; make install_jsregexp"
-    or nil,
-  dependencies = {
-    "rafamadriz/friendly-snippets",
-    config = function()
-      require("luasnip.loaders.from_vscode").lazy_load()
+  {
+    "L3MON4D3/LuaSnip",
+    keys = function()
+      return {}
     end,
   },
-  opts = {
-    history = true,
-    delete_check_events = "TextChanged",
-  },
-  -- stylua: ignore
-  keys = {}
-,
 }
