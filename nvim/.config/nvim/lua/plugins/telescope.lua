aa1f7374601bef0b04508cbc56dfad8a9664efff
Daniel Cox <63171098+dan-kc@users.noreply.github.com>
2023-06-16 20:07:06 +0100

    change prompt location

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -1,6 +1,21 @@
 local Util = require("lazyvim.util")
 return {
   "nvim-telescope/telescope.nvim",
+  dependencies = {
+    "nvim-telescope/telescope-fzf-native.nvim",
+    build = "make",
+    config = function()
+      require("telescope").load_extension("fzf")
+    end,
+  },
+  opts = {
+    defaults = {
+      layout_strategy = "horizontal",
+      layout_config = { prompt_position = "top" },
+      sorting_strategy = "ascending",
+      winblend = 0,
+    },
+  },
   keys = {
     -- disable the keymap to grep files
     { "<leader><space>", false },
