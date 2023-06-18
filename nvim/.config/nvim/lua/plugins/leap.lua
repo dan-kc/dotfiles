e55645e610ce66b02a77d4f4f1ba4e94e0fcbea0
Daniel Cox <63171098+dan-kc@users.noreply.github.com>
2023-06-18 21:59:33 +0100

    fix surround bug

--- nvim/.config/nvim/lua/plugins/leap.lua
+++ nvim/.config/nvim/lua/plugins/leap.lua
@@ -17,4 +17,14 @@ return {
     },
     { "gs", false },
   },
+  config = function(_, opts)
+    local leap = require("leap")
+    for k, v in pairs(opts) do
+      leap.opts[k] = v
+    end
+    leap.add_default_mappings(true)
+    vim.keymap.del({ "x", "o" }, "x")
+    vim.keymap.del({ "x", "o" }, "X")
+    vim.keymap.del({ "n", "x", "o" }, "gs")
+  end,
 }
