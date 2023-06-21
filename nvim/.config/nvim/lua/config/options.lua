a72f88ddeecd270c44b198c1462f834dafbdd18d
Daniel Cox <63171098+dan-kc@users.noreply.github.com>
2023-06-21 18:12:34 +0100

    remove annoying comment continuation

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -1,9 +1,22 @@
--- Options are automatically loaded before lazy.nvim startup
--- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
--- Add any additional options here
-
 local opt = vim.opt
+
 opt.number = false -- Print line number
 opt.relativenumber = false
 opt.cursorline = false
 opt.undofile = true
+
+-- Reduce command buffer size
+opt.cmdwinheight = 1
+
+-- Folding (nvim-ufo)
+opt.foldcolumn = "0"
+opt.foldlevel = 99
+opt.foldlevelstart = 99
+opt.foldenable = true
+
+-- Jumplist (portal.nvim)
+opt.jumpoptions = "stack"
+
+-- Disable comment continuation
+opt.formatoptions:remove("o") -- Don't continue after 'o' or 'O'
+opt.formatoptions:remove("r") -- Don't continue after <cr>
