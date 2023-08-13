90ded54d09617f4e023f9e797947b5791942fc47
Daniel Cox <63171098+dan-kc@users.noreply.github.com>
2023-08-13 18:17:55 +0100

    add back which-key

--- nvim/.config/nvim/lua/plugins/which-key.lua
+++ nvim/.config/nvim/lua/plugins/which-key.lua
@@ -1,34 +1,9 @@
--- Add time delay
-
--- local Util = require("lazyvim.util")
 return {
   "folke/which-key.nvim",
-  enabled = false,
-  --   local wk = require("which-key")
-  --   vim.o.timeout = true
-  --   vim.o.timeoutlen = 1600
-  --   wk.setup(opts)
-  --   local keymaps = {
-  --     mode = { "n", "v" },
-  --     ["g"] = { name = "+goto" },
-  --     ["gs"] = { name = "+surround" },
-  --     ["]"] = { name = "+next" },
-  --     ["["] = { name = "+prev" },
-  --     ["<leader>q"] = { name = "+qickfix" },
-  --     ["<leader>b"] = { name = "+buffer" },
-  --     ["<leader>c"] = { name = "+code" },
-  --     ["<leader>f"] = { name = "+file/find" },
-  --     ["<leader>g"] = { name = "+git" },
-  --     ["<leader>gh"] = { name = "+hunks" },
-  --     ["<leader>h"] = { name = "+harpoon" },
-  --     ["<leader>s"] = { name = "+search" },
-  --     ["<leader>u"] = { name = "+ui" },
-  --     ["<leader>w"] = { name = "+windows" },
-  --     ["<leader>x"] = { name = "+diagnostics/quickfix" },
-  --   }
-  --   if Util.has("noice.nvim") then
-  --     keymaps["<leader>sn"] = { name = "+noice" }
-  --   end
-  --   wk.register(keymaps)
-  -- end,
+  opts = {
+    defaults = {
+      ["gs"] = { name = "+Surround" },
+      ["<leader>h"] = { name = "+harpoon" },
+    },
+  },
 }
