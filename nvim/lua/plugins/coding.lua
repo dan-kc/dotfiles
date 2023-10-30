3a161af3f97dda70e0a47cfba6defa6b4a2005f4
Daniel Cox <63171098+dan-kc@users.noreply.github.com>
2023-10-30 20:19:09 +0000

    fix autobrackets

--- nvim/lua/plugins/coding.lua
+++ nvim/lua/plugins/coding.lua
@@ -55,6 +55,33 @@ return {
   {
     "echasnovski/mini.pairs",
     event = "VeryLazy",
+    opts = {
+      -- No need to copy this inside `setup()`. Will be used automatically.
+      {
+        -- In which modes mappings from this `config` should be created
+        modes = { insert = true, command = false, terminal = false },
+        -- Global mappings. Each right hand side should be a pair information, a
+        -- table with at least these fields (see more in |MiniPairs.map|):
+        -- - <action> - one of 'open', 'close', 'closeopen'.
+        -- - <pair> - two character string for pair to be used.
+        -- By default pair is not inserted after `\`, quotes are not recognized by
+        -- `<CR>`, `'` does not insert pair after a letter.
+        -- Only parts of tables can be tweaked (others will use these defaults).
+        mappings = {
+          ["("] = { action = "open", pair = "()", neigh_pattern = "[^\\]." },
+          ["["] = { action = "open", pair = "[]", neigh_pattern = "[^\\]." },
+          ["{"] = { action = "open", pair = "{}", neigh_pattern = "[^\\]." },
+
+          [")"] = { action = "close", pair = "()", neigh_pattern = "[^\\]." },
+          ["]"] = { action = "close", pair = "[]", neigh_pattern = "[^\\]." },
+          ["}"] = { action = "close", pair = "{}", neigh_pattern = "[^\\]." },
+
+          ['"'] = { action = "closeopen", pair = '""', neigh_pattern = "[^\\].", register = { cr = false } },
+          ["'"] = { action = "closeopen", pair = "''", neigh_pattern = "[^%a\\].", register = { cr = false } },
+          ["`"] = { action = "closeopen", pair = "``", neigh_pattern = "[^\\].", register = { cr = false } },
+        },
+      },
+    },
   },
 
   {
@@ -64,6 +91,7 @@ return {
     config = function()
       require("luasnip.loaders.from_vscode").lazy_load({ paths = "~/.config/nvim/snippets" })
       local ls = require("luasnip")
+    -- TODO: Fix error in ts files when using <c-z> twice
       vim.keymap.set({ "i" }, "<C-z>", function() ls.expand() end, { silent = true })
       vim.keymap.set({ "i", "s" }, "<C-.>", function() ls.jump(1) end, { silent = true })
       vim.keymap.set({ "i", "s" }, "<C-,>", function() ls.jump(-1) end, { silent = true })
