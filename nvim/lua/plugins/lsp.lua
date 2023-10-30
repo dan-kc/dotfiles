8b0ab22efd294cff6bf37576ffd2c61cef537546
Daniel Cox <63171098+dan-kc@users.noreply.github.com>
2023-10-30 20:19:35 +0000

    add border to lsp

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -7,6 +7,27 @@ return {
     event = "VeryLazy",
     config = function(_, _)
       local lspconfig = require("lspconfig")
+
+      -- Add a border to lsp stuff
+      vim.cmd([[autocmd! ColorScheme * highlight NormalFloat guibg=#1f2335]])
+      vim.cmd([[autocmd! ColorScheme * highlight FloatBorder guifg=white guibg=#1f2335]])
+      local border = {
+        { "🭽", "FloatBorder" },
+        { "▔", "FloatBorder" },
+        { "🭾", "FloatBorder" },
+        { "▕", "FloatBorder" },
+        { "🭿", "FloatBorder" },
+        { "▁", "FloatBorder" },
+        { "🭼", "FloatBorder" },
+        { "▏", "FloatBorder" },
+      }
+      local orig_util_open_floating_preview = vim.lsp.util.open_floating_preview
+      function vim.lsp.util.open_floating_preview(contents, syntax, opts, ...)
+        opts = opts or {}
+        opts.border = opts.border or border
+        return orig_util_open_floating_preview(contents, syntax, opts, ...)
+      end
+
       -- brew install lua_ls
       lspconfig.lua_ls.setup({
         settings = {
