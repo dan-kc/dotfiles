fab60acf87c72c19d0b6b43d30e5a227e58d51d3
Daniel Cox <63171098+dan-kc@users.noreply.github.com>
2023-11-02 11:14:43 +0000

    neotree

--- nvim/lua/plugins/editor.lua
+++ nvim/lua/plugins/editor.lua
@@ -1,3 +1,4 @@
+local icons = require("config.icons")
 return {
   {
     "nvim-telescope/telescope.nvim",
@@ -25,6 +26,7 @@ return {
         { "<leader>sM", "<cmd>Telescope man_pages<cr>", desc = "Man Pages" },
         { "<leader>so", "<cmd>Telescope vim_options<cr>", desc = "Options" },
         { "<leader>'", "<cmd>Telescope resume<cr>", desc = "Resume" },
+        { "<leader>ss", function() builtin.lsp_document_symbols() end, desc = "Symbols" },
         { "<leader>sg", function() builtin.live_grep({ cwd = false }) end, desc = "Grep" },
         { "<leader>/", function() builtin.oldfiles({ cwd = vim.loop.cwd() }) end, desc = "Recent" },
        -- { "<leader>fn", function() end, desc = "Find note", }, 
@@ -78,7 +80,196 @@ return {
   },
 
   {
+    "nvim-neo-tree/neo-tree.nvim",
+    event = "VeryLazy",
+    keys = {
+      {
+        "<leader>e",
+        function()
+          require("neo-tree.command").execute({
+            toggle = true,
+            dir = vim.loop.cwd(),
+            position = "current",
+            reveal = true,
+          })
+        end,
+        desc = "Explorer NeoTree",
+      },
+    },
+    deactivate = function()
+      vim.cmd([[Neotree close]])
+    end,
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons",
+      "MunifTanjim/nui.nvim",
+    },
+    opts = {
+      default_component_configs = {
+        container = {
+          enable_character_fade = true,
+        },
+        indent = {
+          indent_size = 2,
+          padding = 1,
+          with_markers = true,
+          indent_marker = "│",
+          last_indent_marker = "└",
+          highlight = "NeoTreeIndentMarker",
+          with_expanders = true,
+          expander_collapsed = icons.ui.TriangleShortArrowRight,
+          expander_expanded = icons.ui.TriangleShortArrowDown,
+          expander_highlight = "NeoTreeExpander",
+        },
+        icon = {
+          folder_closed = icons.ui.Folder,
+          folder_open = icons.ui.FolderOpen,
+          folder_empty = icons.ui.EmptyFolder,
+          default = icons.ui.File,
+          highlight = "NeoTreeFileIcon",
+        },
+        modified = {
+          symbol = icons.git.LineModified,
+          highlight = "NeoTreeModified",
+        },
+        name = {
+          trailing_slash = false,
+          use_git_status_colors = true,
+          highlight = "NeoTreeFileName",
+        },
+        git_status = {
+          symbols = {
+            -- Change type
+            added = icons.git.LineAdded,
+            modified = icons.git.LineModified,
+            deleted = "✖",
+            renamed = "󰁕",
+            -- Status type
+            untracked = icons.git.FileUntracked,
+            ignored = icons.git.FileIgnored,
+            unstaged = icons.git.FileUnstaged,
+            staged = icons.git.FileStaged,
+            conflict = icons.git.FileUnmerged,
+          },
+        },
+        file_size = {
+          enabled = true,
+          required_width = 64, -- min width of window required to show this column
+        },
+        type = {
+          enabled = true,
+          required_width = 122, -- min width of window required to show this column
+        },
+        last_modified = {
+          enabled = true,
+          required_width = 88, -- min width of window required to show this column
+        },
+        created = {
+          enabled = true,
+          required_width = 110, -- min width of window required to show this column
+        },
+        symlink_target = {
+          enabled = false,
+        },
+      },
+      mappings = {
+        ["<cr>"] = "open",
+        ["S"] = "open_split",
+        ["s"] = "open_vsplit",
+        ["C"] = "close_node",
+        ["z"] = "close_all_nodes",
+        ["Z"] = "expand_all_nodes",
+        ["a"] = { "add", config = { show_path = "relative" } },
+        ["A"] = "add_directory",
+        ["d"] = "delete",
+        ["r"] = "rename",
+        ["y"] = "copy_to_clipboard",
+        ["x"] = "cut_to_clipboard",
+        ["p"] = "paste_from_clipboard",
+        ["c"] = "copy", -- takes text input for destination, also accepts the optional config.show_path option like "add":
+        -- ["c"] = { "copy", config = { show_path = "none" -- "none", "relative", "absolute" }}
+        ["m"] = "move", -- takes text input for destination, also accepts the optional config.show_path option like "add".
+        ["q"] = "close_window",
+        ["R"] = "refresh",
+        ["?"] = "show_help",
+        ["<"] = "prev_source",
+        [">"] = "next_source",
+        ["i"] = "show_file_details",
+      },
+      filesystem = {
+        filtered_items = {
+          visible = false, -- when true, they will just be displayed differently than normal items
+          hide_dotfiles = false,
+          hide_gitignored = false,
+          hide_hidden = false,
+          never_show = {
+            --".DS_Store",
+          },
+        },
+        follow_current_file = {
+          enabled = true,
+          leave_dirs_open = true,
+        },
+        group_empty_dirs = false,
+        hijack_netrw_behavior = "open_default",
+        window = {
+          mappings = {
+            ["<bs>"] = "navigate_up",
+            ["."] = "set_root",
+            ["H"] = "toggle_hidden",
+            ["/"] = "fuzzy_finder",
+            ["D"] = "fuzzy_finder_directory",
+            ["#"] = "fuzzy_sorter", -- fuzzy sorting using the fzy algorithm
+            -- ["D"] = "fuzzy_sorter_directory",
+            ["f"] = "filter_on_submit",
+            ["<c-x>"] = "clear_filter",
+            ["[g"] = "prev_git_modified",
+            ["]g"] = "next_git_modified",
+            ["o"] = { "show_help", nowait = false, config = { title = "Order by", prefix_key = "o" } },
+            ["oc"] = { "order_by_created", nowait = false },
+            ["od"] = { "order_by_diagnostics", nowait = false },
+            ["og"] = { "order_by_git_status", nowait = false },
+            ["om"] = { "order_by_modified", nowait = false },
+            ["on"] = { "order_by_name", nowait = false },
+            ["os"] = { "order_by_size", nowait = false },
+            ["ot"] = { "order_by_type", nowait = false },
+          },
+          fuzzy_finder_mappings = { -- define keymaps for filter popup window in fuzzy_finder_mode
+            ["<down>"] = "move_cursor_down",
+            ["<C-n>"] = "move_cursor_down",
+            ["<up>"] = "move_cursor_up",
+            ["<C-p>"] = "move_cursor_up",
+          },
+        },
+      },
+      buffers = {
+        follow_current_file = {
+          enabled = true, -- This will find and focus the file in the active buffer every time
+          leave_dirs_open = true, -- `false` closes auto expanded dirs, such as with `:Neotree reveal`
+        },
+        group_empty_dirs = true,
+        show_unloaded = true,
+        window = {
+          mappings = {
+            ["bd"] = "buffer_delete",
+            ["<bs>"] = "navigate_up",
+            ["."] = "set_root",
+            ["o"] = { "show_help", nowait = false, config = { title = "Order by", prefix_key = "o" } },
+            ["oc"] = { "order_by_created", nowait = false },
+            ["od"] = { "order_by_diagnostics", nowait = false },
+            ["om"] = { "order_by_modified", nowait = false },
+            ["on"] = { "order_by_name", nowait = false },
+            ["os"] = { "order_by_size", nowait = false },
+            ["ot"] = { "order_by_type", nowait = false },
+          },
+        },
+      },
+    },
+  },
+
+  {
     "stevearc/oil.nvim",
+    enabled = false,
     keys = {
       { "<backspace>", "<cmd>Oil<cr>", desc = "Open parent directory" },
     },
@@ -116,7 +307,6 @@ return {
         ["~"] = "actions.tcd",
         ["g."] = "actions.toggle_hidden",
       },
-      use_default_keymaps = true,
     },
     -- Optional dependencies
     dependencies = { "nvim-tree/nvim-web-devicons" },
@@ -147,6 +337,7 @@ return {
           graphql = { "prettier" },
           handlebars = { "prettier" },
           go = { "goimports" },
+          c = { "clang_format" },
         },
       }
     end,
