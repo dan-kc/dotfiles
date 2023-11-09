5eb806e52006d69cee534b4c8979d9298aff87ee
Daniel Cox <63171098+dan-kc@users.noreply.github.com>
2023-11-09 20:36:41 +0000

    mini.pairs => autopairs

--- nvim/lua/plugins/coding.lua
+++ nvim/lua/plugins/coding.lua
@@ -88,26 +88,28 @@ return {
   },
 
   --  ╭──────────────────────────────────────────────────────────╮
-  --  │                        mini.pairs                        │
+  --  │                      nvim-autopairs                      │
   --  ╰──────────────────────────────────────────────────────────╯
   {
-    "echasnovski/mini.pairs",
+    "windwp/nvim-autopairs",
     event = "VeryLazy",
     opts = {
-      {
-        modes = { insert = true, command = false, terminal = false },
-        mappings = {
-          ["("] = { action = "open", pair = "()", neigh_pattern = "[^\\]." },
-          ["["] = { action = "open", pair = "[]", neigh_pattern = "[^\\]." },
-          ["{"] = { action = "open", pair = "{}", neigh_pattern = "[^\\]." },
-          [")"] = { action = "close", pair = "()", neigh_pattern = "[^\\]." },
-          ["]"] = { action = "close", pair = "[]", neigh_pattern = "[^\\]." },
-          ["}"] = { action = "close", pair = "{}", neigh_pattern = "[^\\]." },
-          ['"'] = { action = "closeopen", pair = '""', neigh_pattern = "[^\\].", register = { cr = false } },
-          ["'"] = { action = "closeopen", pair = "''", neigh_pattern = "[^%a\\].", register = { cr = false } },
-          ["`"] = { action = "closeopen", pair = "``", neigh_pattern = "[^\\].", register = { cr = false } },
-        },
-      },
+      disable_filetype = { "TelescopePrompt" },
+      disable_in_macro = true, -- disable when recording or executing a macro,
+      disable_in_visualblock = false, -- disable when insert after visual block mode
+      disable_in_replace_mode = true,
+      ignored_next_char = [=[[%w%%%'%[%"%.%`%$]]=],
+      enable_moveright = true,
+      enable_afterquote = true, -- add bracket pairs after quote
+      enable_check_bracket_line = true, --- check bracket in same line
+      enable_bracket_in_quote = true, --
+      enable_abbr = false, -- trigger abbreviation
+      break_undo = true, -- switch for basic rule break undo sequence
+      check_ts = true,
+      map_cr = true,
+      map_bs = false, -- map the <BS> key
+      map_c_h = false, -- Map the <C-h> key to delete a pair
+      map_c_w = false, -- map <c-w> to delete a pair if possible
     },
   },
 
