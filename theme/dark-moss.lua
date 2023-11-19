7583fbb99297d9dbe5abcdf52cf3d2fb45c18711
Daniel Cox <63171098+dan-kc@users.noreply.github.com>
2023-11-19 10:22:32 +0000

    add dark moss

---
+++ theme/dark-moss.lua
@@ -0,0 +1,26 @@
+return {
+	"Dark moss",
+	settings = {
+		dark = true,
+		opacity = 0.9,
+	},
+	normal = {
+		{ black = "0x171e1f" },
+		{ red = "0xff4658" },
+		{ green = "0x499180" },
+		{ yellow = "0xfdb11f" },
+		{ blue = "0x498091" },
+		{ magenta = "0x9bc0c8" },
+		{ cyan = "0x66d9ef" },
+		{ white = "0xc7c7a5" },
+	},
+	brights = {
+		{ black = "0x555e5f" },
+		{ red = "0xe6db74" },
+		{ green = "0x252c2d" },
+		{ yellow = "0x373c3d" },
+		{ blue = "0x818f80" },
+		{ magenta = "0xe3e3c8" },
+		{ cyan = "0xd27b53" },
+	},
+}
