eee6da66d3f36d3a5a1d4b537c3ed74460b67210
Daniel Cox <63171098+dan-kc@users.noreply.github.com>
2023-12-07 21:21:53 +0000

    hurmit font

--- wezterm/font.lua
+++ wezterm/font.lua
@@ -1,6 +1,4 @@
 local wezterm = require("wezterm")
--- font = wezterm.font("Fira Code")
-return wezterm.font("Hurmit Nerd Font")
--- font = wezterm.font("FiraCode Nerd Font")
+-- return wezterm.font("Hurmit Nerd Font")
+return wezterm.font("FiraCode Nerd Font")
 -- return wezterm.font("Mononoki Nerd Font")
--- font = wezterm.font("JetBrains Mo")
