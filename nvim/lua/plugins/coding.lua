73c4a9e02325d084886e2d6a2a23792d8bd99574
Daniel Cox <63171098+dan-kc@users.noreply.github.com>
2023-12-21 19:12:37 +0000

    rm format on save

--- nvim/lua/plugins/coding.lua
+++ nvim/lua/plugins/coding.lua
@@ -179,13 +179,21 @@ return {
     "stevearc/conform.nvim",
     -- enabled = false,
     event = "VeryLazy",
+    keys = {
+      {
+        "<leader>a",
+        function()
+          require("conform").format({ async = true, lsp_fallback = false })
+        end,
+        desc = "Format buffer",
+      },
+    },
     opts = function()
       return {
-        notify_on_error = false,
-        format_on_save = {
-          timeout_ms = 300,
-          lsp_fallback = false, -- stops formatting when no formatters are listed in formatters_by_ft
-        },
+        -- format_on_save = {
+        --   timeout_ms = 300,
+        --   lsp_fallback = false, -- stops formatting when no formatters are listed in formatters_by_ft
+        -- },
         formatters_by_ft = {
           -- typescript = { "prettier" },
           -- tsx = { "prettier" },
