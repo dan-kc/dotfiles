ed932a77dd9ef12b1577c7db35420189671b5cc4
Daniel Cox <63171098+dan-kc@users.noreply.github.com>
2024-01-29 14:28:24 +0000

    better format binding

--- nvim/lua/plugins/coding.lua
+++ nvim/lua/plugins/coding.lua
@@ -103,17 +103,16 @@ return {
       disable_in_macro = true, -- disable when recording or executing a macro,
       disable_in_visualblock = false, -- disable when insert after visual block mode
       disable_in_replace_mode = true,
-      -- ignored_next_char = [=[[%w%%%'%[%"%.%`%$]]=],
+    -- ignored_next_char = [=[[%w%%%'%[%"%.%`%$]]=],
       enable_moveright = true,
       enable_afterquote = true, -- add bracket pairs after quote
-      -- enable_check_bracket_line = true, --- check bracket in same line
       enable_check_bracket_line = false, --- check bracket in same line
       enable_bracket_in_quote = false, --
       enable_abbr = false, -- trigger abbreviation
       break_undo = true, -- switch for basic rule break undo sequence
       check_ts = true,
       map_cr = true,
-      map_bs = true, -- map the <BS> key
+      map_bs = false, -- map the <BS> key
       map_c_h = true, -- Map the <C-h> key to delete a pair
       map_c_w = false, -- map <c-w> to delete a pair if possible
     },
@@ -181,7 +180,7 @@ return {
     event = "VeryLazy",
     keys = {
       {
-        "f",
+        "<leader>.",
         function()
           require("conform").format({ async = true, lsp_fallback = false })
         end,
