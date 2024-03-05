6a72c6eb7f2c5b9b6deedd205947ee44ed8898b8
Daniel Cox <63171098+dan-kc@users.noreply.github.com>
2024-03-05 19:52:04 +0000

    rust analyzer and formatter

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -105,7 +105,31 @@ return {
       })
 
       -- brew install rust-analyzer
-      lspconfig.rust_analyzer.setup({})
+      lspconfig.rust_analyzer.setup({
+        settings = {
+          ["rust-analyzer"] = {
+            cargo = {
+              allFeatures = true,
+              loadOutDirsFromCheck = true,
+              runBuildScripts = true,
+            },
+            -- Add clippy lints for Rust.
+            checkOnSave = {
+              allFeatures = true,
+              command = "clippy",
+              extraArgs = { "--no-deps" },
+            },
+            procMacro = {
+              enable = true,
+              ignored = {
+                ["async-trait"] = { "async_trait" },
+                ["napi-derive"] = { "napi" },
+                ["async-recursion"] = { "async_recursion" },
+              },
+            },
+          },
+        },
+      })
 
       -- go install golang.org/x/tools/gopls@latest
       lspconfig.gopls.setup({
