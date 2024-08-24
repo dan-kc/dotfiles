8d1f668dfea01f1fcc765d169b93897a27345298
Daniel Cox <63171098+dan-kc@users.noreply.github.com>
2024-08-24 23:21:57 +0100

    update with next/prev tab

--- monkey_script.js
+++ monkey_script.js
@@ -1,55 +1,60 @@
 // ==UserScript==
 // @name        Prevent keyboard shortcut hijacking
-// @description Prevent websites to hijack keyboard shortcuts, for example <Ctrl+F> on Discourse
+// @description Personal config to prevent certain keyboard hijacking
 // @namespace   Violentmonkey Scripts
 // @match       *://*/*
 // @grant       none
 // @version     1.0
-// @author      notDavid
+// @author      Daniel
 // ==/UserScript==
 
-(window.opera ? document.body : document).addEventListener('keydown', function(e) {
-  //alert(e.keyCode ); //uncomment to find more keyCodes
+(window.opera ? document.body : document).addEventListener(
+  "keydown",
+  function (e) {
+    // alert(e.keyCode );
 
-  var OS="Unknown";
-  if (navigator.appVersion.toUpperCase().indexOf("MAC")>=0) OS="macOS";
-  //if (navigator.appVersion.toUpperCase().indexOf("LINUX")>=0) OS="Linux";
-  //if (navigator.appVersion.toUpperCase().indexOf("X11")>=0) OS="UNIX";
-  //if (navigator.appVersion.toUpperCase().indexOf("WIN")>=0) OS="Windows";
-  //console.log(OS);
+    // var OS = "Unknown";
+    // if (navigator.appVersion.toUpperCase().indexOf("MAC") >= 0) OS = "macOS";
+    //if (navigator.appVersion.toUpperCase().indexOf("LINUX")>=0) OS="Linux";
+    //if (navigator.appVersion.toUpperCase().indexOf("X11")>=0) OS="UNIX";
+    //if (navigator.appVersion.toUpperCase().indexOf("WIN")>=0) OS="Windows";
+    //console.log(OS);
 
-  // macOS uses the Command key, identified as metaKey
-  // Windows and Linux use the Control key, identified as ctrlKey
-  //var modifier = (OS == "macOS") ? e.metaKey : e.ctrlKey;
-  var modifier_cmd = e.metaKey;
-  var modifier_ctrl = e.ctrlKey;
+    var modifier_cmd = e.metaKey;
+    var modifier_ctrl = e.ctrlKey;
+    var modifier_alt = e.altKey;
+    var modifier_shift = e.shiftKey;
 
-  if (e.keyCode === 116) {
-      // F5 should never be captured
+    var no_mods =
+      !modifier_shift && !modifier_ctrl && !modifier_alt && !modifier_cmd;
+    if (no_mods) {
+      return;
+    }
+
+    // F5
+    if (e.keyCode === 116) {
       e.stopImmediatePropagation();
       return;
-  }
+    }
 
-  // abort if modifier isn't pressed
-  if (modifier_cmd || modifier_ctrl) {
-
-    if (modifier_ctrl) {
+    if (modifier_cmd && modifier_alt) {
       switch (e.keyCode) {
-        case 70: // f - find
+        case 37: // <- - tab left
+        case 39: // -> - tab right
           e.stopImmediatePropagation();
           return;
       }
     }
 
-    if ((OS == "macOS") && (modifier_cmd)) {
+    if (modifier_cmd) {
       switch (e.keyCode) {
         case 188: // , - settings
-        case 82:  // r - reload
-        case 70:  // f - find (Discourse)
+        case 82: // r - reload
+        case 70: // f - find
+        case 191: // / - command menu
           e.stopImmediatePropagation();
           return;
       }
     }
-  }
-
-}, true);
+  },
+  true,
