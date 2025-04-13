66cab31ed63bfc456ae7350ec2b53726a6f2962a
Daniel Cox <daniel@keone.dev>
2025-04-13 14:41:08 +0100

    add more wifi creds

--- system/networking.nix
+++ system/networking.nix
@@ -16,6 +16,10 @@
       secretsFile = config.sops.secrets."wifi.env".path;
       networks = {
         "Redefined".pskRaw = "ext:COWORK_PSK";
+        "M-D-HOME".psk = "ext:HOME_PSK";
+        "SP OFFICE 5G".psk = "ext:SP_OFFICE_PSK";
+        "SP OFFICE 2G".psk = "ext:SP_OFFICE_PSK";
+        "SP Upstairs WiFi".psk = "ext:SP_OFFICE_UPSTAIRS_PSK";
       };
     };
   };
