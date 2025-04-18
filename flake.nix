12fc597799db7cf6aa8622b21f26c64ff7d08721
Daniel Cox <daniel@keone.dev>
2025-04-18 22:12:20 +0100

    lock zen to a commit & rm fonts wezterm

--- flake.nix
+++ flake.nix
@@ -4,17 +4,20 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     # nixpkgs.url = "github:NixOS/nixpkgs/release-24.11";
-    home-manager = {
-      url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
     neovim.url = "github:dan-kc/neovim";
     wezterm.url = "github:wez/wezterm?dir=nix";
-    zen-browser.url = "github:0xc000022070/zen-browser-flake";
     yazi.url = "github:sxyazi/yazi";
     sops-nix.url = "github:Mic92/sops-nix";
     hyprland.url = "github:hyprwm/Hyprland";
     xremap-flake.url = "github:xremap/nix-flake";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    zen-browser = {
+      url = "github:0xc000022070/zen-browser-flake/805c8f56e8ebac1527176fc9d551f73c4cd886f6";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs =
