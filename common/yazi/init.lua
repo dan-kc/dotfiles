1e1334ef339f44591810b2efc0202372667ef065
Daniel Cox <daniel@keone.dev>
2025-04-25 11:54:55 +0100

    yazi upgrades

--- common/yazi/init.lua
+++ common/yazi/init.lua
@@ -12,3 +12,21 @@ Status:children_add(function()
 		" ",
 	}
 end, 500, Status.RIGHT)
+
+-- Show symlink in the status bar
+Status:children_add(function(self)
+	local h = self._current.hovered
+	if h and h.link_to then
+		return " -> " .. tostring(h.link_to)
+	else
+		return ""
+	end
+end, 3300, Status.LEFT)
+
+-- Show username and hostname in header
+Header:children_add(function()
+	if ya.target_family() ~= "unix" then
+		return ""
+	end
+	return ui.Span(ya.user_name() .. "@" .. ya.host_name() .. ":"):fg("blue")
+end, 500, Header.LEFT)
