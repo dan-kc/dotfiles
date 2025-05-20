1792b222a13d194d6e440f24dc9483b8e6266284
Daniel Cox <63171098+dan-kc@users.noreply.github.com>
2025-05-20 17:10:02 +0100

    add nix cache for hyprland

--- system/common/default.nix
+++ system/common/default.nix
@@ -56,6 +56,11 @@
 
   console.font = "Lat2-Terminus16";
 
+  nix.settings = {
+    substituters = ["https://hyprland.cachix.org"];
+    trusted-substituters = ["https://hyprland.cachix.org"];
+    trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
+  };
   virtualisation.docker.enable = true;
 
   nixpkgs.config.allowUnfree = true;
