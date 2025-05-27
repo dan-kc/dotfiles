677d708633fe8cf3aafc34d638f97acc4626145f
Daniel Cox <63171098+dan-kc@users.noreply.github.com>
2025-05-27 09:42:17 +0100

    add path arg to theme-switch

--- home/theme.nix
+++ home/theme.nix
@@ -12,6 +12,18 @@ pkgs.stdenv.mkDerivation {
     # Randomly pick a flie in /wallpapers
     selected_file=$(find ~/wallpapers/dark ~/wallpapers/light -type f -print0 | shuf -z -n 1 | xargs -0)
 
+    # Check if an argument (absolute path) is provided
+    if [ "$#" -ge 1 ]; then
+      # Use the provided argument as the selected file
+      selected_file="$1"
+
+      # Optional: Add a check to ensure the provided path is a valid file
+      if [ ! -f "$selected_file" ]; then
+        echo "Error: Provided path '$selected_file' is not a valid file."
+        exit 1
+      fi
+    fi
+
     ${pkgs.flavours}/bin/flavours generate dark "$selected_file"
     ${pkgs.flavours}/bin/flavours apply generated
 
