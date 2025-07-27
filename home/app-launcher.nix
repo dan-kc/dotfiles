c1bbe17243449e05cfe943cc8095e8779e69cb9a
Daniel Cox <63171098+dan-kc@users.noreply.github.com>
2025-07-27 19:21:02 +0100

    add obsidian to app launcher

--- home/app-launcher.nix
+++ home/app-launcher.nix
@@ -9,7 +9,7 @@ pkgs.stdenv.mkDerivation {
   src = pkgs.writeShellScript "launcher-script" ''
     #!/bin/sh
 
-    apps="bruno\nvivaldi\nslack\nanki\ndiscord\npostman\nthunderbird\nalacritty\ncursor\nsunsama\npavucontrol"
+    apps="bruno\nvivaldi\nslack\nanki\ndiscord\npostman\nthunderbird\nalacritty\ncursor\nsunsama\npavucontrol\nobsidian"
 
     # Use printf for better compatibility
     selected_app=$(printf "$apps" | bemenu -i -p "Launch Application:")
