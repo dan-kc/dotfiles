0926c2b07e0a7dfac26239584fd61085763f08ba
Daniel Cox <63171098+dan-kc@users.noreply.github.com>
2025-12-22 08:39:14 +0000

    fix home-manager mac

--- home/macos/default.nix
+++ home/macos/default.nix
@@ -15,6 +15,11 @@
     "Library/Application\ Support/lazygit/config.yml".source = ../common/lazygit.yml;
   };
 
+  nixpkgs.config.allowUnfreePredicate =
+    pkg:
+    builtins.elem (lib.getName pkg) [
+      "claude-code"
+    ];
 
   # Allow home-manager to manage home-manager.
   programs.home-manager.enable = true;
