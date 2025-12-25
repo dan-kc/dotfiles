a88934b601dae15c43e296206ca785cb6571cc64
Daniel Cox <63171098+dan-kc@users.noreply.github.com>
2025-12-25 11:14:33 +0000

    reload on config change

--- home/nixos/notifications.nix
+++ home/nixos/notifications.nix
@@ -9,20 +9,22 @@ let
 in
 {
   home.file = {
-    ".config/mako/config".text = ''
-      max-history=4
-      font=${config.globalFonts.selectedFont}
-      background-color=#${config.colorScheme.palette.base00}
-      text-color=#${config.colorScheme.palette.base05}
-      height=140
-      width=320
-      margin=14
-      padding=4
-      border-size=2
-      border-color=#${config.colorScheme.palette.base0C}
-      anchor=top-left
+    ".config/mako/config" = {
+      text = ''
+        max-history=4
+        font=${config.globalFonts.selectedFont}
+        background-color=#${config.colorScheme.palette.base00}
+        text-color=#${config.colorScheme.palette.base05}
+        height=140
+        width=320
+        margin=14
+        padding=4
+        border-size=2
+        border-color=#${config.colorScheme.palette.base0C}
+        anchor=top-left
       '';
-
+      onChange = "${pkgs.mako}/bin/makoctl reload || true";
+    };
   };
   home.packages = with pkgs; [
     libnotify
