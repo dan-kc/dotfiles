213416db1b115ba2a316af3b797ff457df8a45c5
Daniel Cox <63171098+dan-kc@users.noreply.github.com>
2025-12-25 10:03:54 +0000

    rm waybar

--- system/common/default.nix
+++ system/common/default.nix
@@ -107,7 +107,6 @@
     swaylock
     swayidle
   ];
-  programs.waybar.enable = true;
   services.gnome.gnome-keyring.enable = true;
   security.polkit.enable = true;
   security.pam.services.swaylock = {};
