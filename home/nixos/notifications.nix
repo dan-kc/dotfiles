dcbfe55d1872c414d09d37505f42acbaa4ed1fd2
Daniel Cox <63171098+dan-kc@users.noreply.github.com>
2025-12-25 10:49:19 +0000

    notifcation styling

--- home/nixos/notifications.nix
+++ home/nixos/notifications.nix
@@ -1,14 +1,27 @@
 {
   pkgs,
+  inputs,
   config,
   ...
 }:
+let
+  hexToRGBString = inputs.nix-colors.lib-core.conversions.hexToRGBString;
+in
 {
   home.file = {
     ".config/mako/config".text = ''
+      max-history=4
       font=${config.globalFonts.selectedFont}
-
-    '';
+      background-color=#${config.colorScheme.palette.base00}
+      text-color=#${config.colorScheme.palette.base05}
+      height=140
+      width=320
+      margin=14
+      padding=4
+      border-size=2
+      border-color=#${config.colorScheme.palette.base0C}
+      anchor=top-left
+      '';
 
   };
   home.packages = with pkgs; [
