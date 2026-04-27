73f587988f9c1ef555953118869fcbbe54da728b
Daniel Cox <63171098+dan-kc@users.noreply.github.com>
2026-04-27 21:19:55 +0100

    configure tailscale

--- system/common/networking.nix
+++ system/common/networking.nix
@@ -16,13 +16,11 @@
       networks = {
         "Redefined".pskRaw = "ext:COWORK_SPACE";
         "M-D-HOME".pskRaw = "ext:HOME";
-        "SP Office 5G".pskRaw = "ext:SP_OFFICE";
-        "BTB-W26CHT".pskRaw = "ext:BTB-W26CHT";
         # "iPhone rass".psk = "ztz785i0frev";
-        "119/32_5G".psk = "@NOSBAAN.h3j";
-        ".@ TrueDigitalPark Free WiFi" = {};
-        "AtlasWorkspace - Guest" = {};
       };
     };
+  };
+  services.tailscale = {
+    enable = true;
   };
 }
