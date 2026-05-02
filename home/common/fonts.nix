44d3f86cfcee02669c68a9e004f50f0f258077e8
Daniel Cox <63171098+dan-kc@users.noreply.github.com>
2026-05-02 09:53:50 +0100

    fix theming

--- home/common/fonts.nix
+++ home/common/fonts.nix
@@ -1,30 +1,108 @@
 {
   pkgs,
   lib,
+  config,
   ...
 }:
-{
-  options.globalFonts.selectedFont = lib.mkOption {
-    type = lib.types.str;
-    default = "Monofur Nerd Font Mono";
-    description = "The name of the globally selected Nerd Font.";
+let
+  # Change this one value when you want to switch fonts.
+  defaultFont = "ubuntu-mono";
+
+  fontCatalog = {
+    # key = exact family name used by apps
+    fira-code = {
+      package = pkgs.nerd-fonts.fira-code;
+      family = "FiraCode Nerd Font Mono";
+    };
+    hurmit = {
+      package = pkgs.nerd-fonts.hurmit;
+      family = "Hurmit Nerd Font Mono";
+    };
+    jetbrains-mono = {
+      package = pkgs.nerd-fonts.jetbrains-mono;
+      family = "JetBrainsMono Nerd Font Mono";
+    };
+    martian-mono = {
+      package = pkgs.nerd-fonts.martian-mono;
+      family = "MartianMono Nerd Font Mono";
+    };
+    monaspace = {
+      package = pkgs.monaspace;
+      family = "Monaspace Neon NF";
+    };
+    monaspace-argon = {
+      package = pkgs.monaspace;
+      family = "Monaspace Argon NF";
+    };
+    monaspace-krypton = {
+      package = pkgs.monaspace;
+      family = "Monaspace Krypton NF";
+    };
+    monaspace-radon = {
+      package = pkgs.monaspace;
+      family = "Monaspace Radon NF";
+    };
+    monaspace-xenon = {
+      package = pkgs.monaspace;
+      family = "Monaspace Xenon NF";
+    };
+    monofur = {
+      package = pkgs.nerd-fonts.monofur;
+      family = "Monofur Nerd Font Mono";
+    };
+    profont = {
+      package = pkgs.profont;
+      family = "ProFont";
+    };
+    proggy-clean = {
+      package = pkgs.proggyfonts;
+      family = "ProggyCleanTT";
+    };
+    proggy-small = {
+      package = pkgs.proggyfonts;
+      family = "ProggySmallTT";
+    };
+    proggy-square = {
+      package = pkgs.proggyfonts;
+      family = "ProggySquareTT";
+    };
+    proggy-tiny = {
+      package = pkgs.proggyfonts;
+      family = "ProggyTinyTT";
+    };
+    space-mono = {
+      package = pkgs.google-fonts;
+      family = "Space Mono";
+    };
+    ubuntu-mono = {
+      package = pkgs.nerd-fonts.ubuntu-mono;
+      family = "UbuntuMono Nerd Font Mono";
+    };
   };
 
-  # List all installed fonts
-  # fc-list : file family style | grep -E "Mono"
-  config = {
-    home.packages = with pkgs; [
-      nerd-fonts.jetbrains-mono # JetBrainsMono Nerd Font Mono 7/10
-      proggyfonts
-      nerd-fonts.geist-mono # GeistMono Nerd Font Mono
-      nerd-fonts.hack # Hack Nerd Font Mono 6/10
-      nerd-fonts.hurmit # Hurmit Nerd Font Propo
-      nerd-fonts.monofur # Monofur Nerd Font Propo 6/10
-      nerd-fonts.fira-code # FiraCode Nerd Font Propo
+  selectedFont = fontCatalog.${config.globalFonts.default};
+in
+{
+  options.globalFonts = {
+    default = lib.mkOption {
+      type = lib.types.enum (builtins.attrNames fontCatalog);
+      default = defaultFont;
+      example = "martian-mono";
+      description = "Font preset to use globally. Change defaultFont in this file.";
+    };
 
-      # Don't work? They don't appear to be downloaded at all.
-      nerd-fonts.space-mono # SpaceMono Nerd Font Propo
-      nerd-fonts.mononoki # Mononoki Nerd Font Propo
+    selectedFont = lib.mkOption {
+      type = lib.types.str;
+      readOnly = true;
+      description = "Resolved font family name used by applications.";
+    };
+  };
+
+  config = {
+    globalFonts.selectedFont = selectedFont.family;
+
+    home.packages = [
+      selectedFont.package
     ];
   };
 }
